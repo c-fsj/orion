55d56b86b2e99fa027f4424e9ab594bb02cf4e37
c-fsj <88323484+c-fsj@users.noreply.github.com>
2021-08-07 19:51:54 +0200

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,6 +7,7 @@ echo "\nProgreso (                                       - 0%)\n"
 
 # Creación de carpeta de software
 
+echo "\nProgreso (==                                     - 5%)\n"
 echo "\nCreando el directorio /mnt/software...\n"
 sudo mkdir /mnt/software
 sudo chmod -R 777 /mnt/software
@@ -16,22 +17,12 @@ cd /mnt/software
 
 # Preparando el entorno
 
-echo "\nProgreso (==                                     - 5%)\n"
+echo "\nProgreso (====                                   - 10%)\n"
 echo "\nInstalando librerías...\n"
 sudo apt-get update
 sudo apt-get install python python-dev python-pip-whl python3 python3-pip build-essential docker.io docker-compose docker-registry testresources default-jdk -y
 
-
-# 1.CREEPY
-
-echo "\nProgreso (====                                   - 10%)\n"
-echo "\nInstalando Creepy...\n"
-sudo apt-get install -y && sudo pip install pytz python-qt flickrapi python-instagram yapsy tweepy google-api-python-client python-dateutil configobj dominate
-sudo git clone https://github.com/ilektrojohn/creepy/tree/master/creepy
-# python CreepyMain.py HACE FALTA CONFIGURARLO PARA QUE CORRA CON PYTHON3
-
-
-# 2. DATASPLOIT
+# 1. DATASPLOIT
 
 echo "\nProgreso (======                                 - 15%)\n"
 echo "\nInstalando DataSploit...\n"
@@ -43,13 +34,13 @@ cd ..
 # python datasploit.py
 
 
-# 3. DMitry
+# 2. DMitry
 
 echo "\nProgreso (========                               - 20%)\n"
 echo "\nInstalando DMitry...\n"
 sudo apt-get install dmitry -y
 
-# 4. ExifTool
+# 3. ExifTool
 
 echo "\nProgreso (==========                             - 25%)\n"
 echo "\nInstalando ExifTool...\n"
@@ -62,7 +53,7 @@ sudo make test
 sudo make install
 cd ..
 
-# 5. FOCA
+# 4. FOCA
 
 echo "\nProgreso (============                           - 30%)\n"
 echo "\nInstalando FOCA...\n"
@@ -71,7 +62,7 @@ sudo winetricks dotnet20 gdiplus fontfix
 # wine 'FOCA Free.exe'
 
 
-# 6. Infoga
+# 5. Infoga
 
 echo "\nProgreso (==============                         - 35%)\n"
 echo "\nInstalando Infoga...\n"
@@ -83,14 +74,14 @@ cd ..
 # python infoga.py
 
 
-# 7. Instagram Scraper
+# 6. Instagram Scraper
 
 echo "\nProgreso (================                       - 40%)\n"
 echo "\nInstalando Instagram Scraper...\n"
 sudo pip3 install instagram-scraper
 
 
-# 8. Maltego
+# 7. Maltego
 
 echo "\nProgreso (==================                     - 45%)\n"
 echo "\nInstalando Maltego...\n"
@@ -98,14 +89,14 @@ wget https://maltego-downloads.s3.us-east-2.amazonaws.com/linux/Maltego.v4.2.19.
 sudo dpkg -i Maltego.v4.2.19.13940.deb
 
 
-# 9. Maryam
+# 8. Maryam
 
 echo "\nProgreso (====================                   - 50%)\n"
 echo "\nInstalando Maryam...\n"
 sudo pip3 install maryam
 
 
-# 10. Metagoofil
+# 9. Metagoofil
 
 echo "\nProgreso (======================                 - 55%)\n"
 echo "\nInstalando Metagoofil...\n"
@@ -115,7 +106,7 @@ sudo pip install -r requirements.txt
 cd ..
 
 
-# 11. OSINT-SPY
+# 10. OSINT-SPY
 
 echo "\nProgreso (========================               - 60%)\n"
 echo "\nInstalando OSINT-SPY...\n"
@@ -124,13 +115,13 @@ cd OSINT-SPY
 sudo pip install -r requirements.txt
 cd ..
 
-# 12. OSRFramework
+# 11. OSRFramework
 
 echo "\nProgreso (==========================             - 65%)\n"
 echo "\nInstalando OSRFramework...\n"
 sudo pip3 install osrframework
 
-# 13. ReconSpider
+# 12. ReconSpider
 
 echo "\nProgreso (============================           - 70%)\n"
 echo "\nInstalando ReconSpider...\n"
@@ -143,14 +134,14 @@ sudo git clone https://github.com/c-fsj/orion/tree/main/tools/reconspider/ip2loc
 perl ip2location-downloader-linux/download.pl
 cd ..\..
 
-# 14. Recon-ng
+# 13. Recon-ng
 
 echo "\nProgreso (==============================         - 75%)\n"
 echo "\nInstalando Recon-ng...\n"
 sudo apt-get install recon-ng -y
 
 
-# 15. Sherlock
+# 14. Sherlock
 
 echo "\nProgreso (================================      - 80%)\n"
 echo "\nInstalando Sherlock...\n"
@@ -161,7 +152,7 @@ cd ..
 # python3 sherlock
 
 
-# 16. Spiderfoot
+# 15. Spiderfoot
 
 echo "\nProgreso (==================================     - 85%)\n"
 echo "\nInstalando Spiderfoot...\n"
@@ -172,14 +163,14 @@ cd ..
 # python3 ./sf.py -l 127.0.0.1:5001
 
 
-# 17. SpyScrap
+# 16. SpyScrap
 
 echo "\nProgreso (====================================   - 90%)\n"
 echo "\nInstalando SpyScrap...\n"
 sudo docker build -t spyscrap .
 # docker run -ti -v /PATH/TO/SpyScrap/src/data:/spyscrap/data spyscrap  [options]
 
-# 18. TheHarvester
+# 17. TheHarvester
 
 echo "\nProgreso (====================================== - 95%)\n"
 echo "\nInstalando TheHarvester...\n"
